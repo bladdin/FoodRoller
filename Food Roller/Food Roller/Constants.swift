e73316f89e365f157bdc6e91eab64040d253e5eb
Benjamin Laddin <benjamin.laddin@gmail.com>
2015-08-24 13:28:25 -0700

    cleaned up random statements for path

--- Food Roller/Food Roller/Constants.swift
+++ Food Roller/Food Roller/Constants.swift
@@ -7,11 +7,25 @@
 //
 
 import Foundation
+
+//Mark: Number of random numbers created
+let kNumberOfRandoms = 5
+
+//MARK: Random Number Constants
+let krandomNumber1 = 1
+let krandomNumber2 = 2
+let krandomNumber3 = 3
+let krandomNumber4 = 4
+let krandomNumber5 = 5
+
+//MARK: Path Lenght Constants
 let kPath1Length = 1
 let kPath2Length = 2
 let kPath3Length = 3
 let kPath4Length = 4
 let kPath5Length = 5
+
+//MARK: Position Divider Constants
 let kPathPosition1Divider = 5
 let kPathPosition2Divider = 10
 let kPathPosition3Divider = 15
--- Food Roller/Food Roller/RandomElements.swift
+++ Food Roller/Food Roller/RandomElements.swift
@@ -14,16 +14,16 @@ class RandomElements{
   
   //MARK: Random Path Length
   class func randomPathLength() -> Int?{
-    var random = Int(arc4random_uniform(5)+1)
-    if random == 1{
+    var random = RandomCaller.createsRandomNumber(kNumberOfRandoms)
+    if random == krandomNumber1{
       return kPath1Length
-    }else if random == 2{
+    }else if random == krandomNumber2{
       return kPath2Length
-    }else if random == 3{
+    }else if random == krandomNumber3{
       return kPath3Length
-    }else if random == 4{
+    }else if random == krandomNumber4{
       return kPath4Length
-    }else if random == 5{
+    }else if random == krandomNumber5{
       return kPath5Length
     }
     else{
@@ -32,16 +32,16 @@ class RandomElements{
     }
   //MARK: Random Path Y Variable Position
   class func randomPathVarYPosition(screenHeight : Int ) -> Int!{
-    var random = Int(arc4random_uniform(5)+1)
-    if random == 1{
+    var random = RandomCaller.createsRandomNumber(kNumberOfRandoms)
+    if random == krandomNumber1{
       return screenHeight / kPathPosition1Divider
-    }else if random == 2{
+    }else if random == krandomNumber2{
       return screenHeight / kPathPosition2Divider
-    }else if random == 3{
+    }else if random == krandomNumber3{
       return screenHeight / kPathPosition3Divider
-    }else if random == 4{
+    }else if random == krandomNumber4{
       return screenHeight / kPathPosition4Divider
-    }else if random == 5{
+    }else if random == krandomNumber5{
       return screenHeight / kPathPosition5Divider
     }
     else{
@@ -51,16 +51,16 @@ class RandomElements{
   
   //MARK: Random Path X Variable Position
   class func randomPathVarxPosition(widthVar : Int ) -> Int!{
-    var random = Int(arc4random_uniform(5)+1)
-    if random == 1{
+    var random = RandomCaller.createsRandomNumber(kNumberOfRandoms)
+    if random == krandomNumber1{
       return widthVar / kPathPosition1Divider
-    }else if random == 2{
+    }else if random == krandomNumber2{
       return widthVar / kPathPosition2Divider
-    }else if random == 3{
+    }else if random == krandomNumber3{
       return widthVar / kPathPosition3Divider
-    }else if random == 4{
+    }else if random == krandomNumber4{
       return widthVar / kPathPosition4Divider
-    }else if random == 5{
+    }else if random == krandomNumber5{
       return widthVar / kPathPosition5Divider
     }
     else{
