17afdb91b881ac04b82d01d983da85d64900ff03
Cathy Oun <cathy810218@gmail.com>
2015-08-24 16:15:45 -0700

    cathys

--- Food Roller/Food Roller/GameScene.swift
+++ Food Roller/Food Roller/GameScene.swift
@@ -9,37 +9,51 @@
 import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-  var background = SKSpriteNode(imageNamed: "background")
   var hotdog = SKSpriteNode()
   var backgroundSpeed : CGFloat = 3
+  
   override func didMoveToView(view: SKView) {
-    
     self.physicsWorld.gravity = CGVectorMake(0, -3)
     self.physicsWorld.contactDelegate = self
-    
     for (var i : CGFloat = 0; i < 2; i++ ) {
       let bg = SKSpriteNode(imageNamed: "background")
+      let groundTexture = SKTexture(imageNamed: "spikes")
+      let spikeNode = SKSpriteNode(texture: groundTexture)
       bg.size = CGSize(width: self.frame.size.width, height: self.frame.size.height)
       bg.anchorPoint = CGPointZero
       bg.position = CGPoint(x: i * bg.size.width, y: 0)
       
       bg.name = "background"
+      spikeNode.size = CGSize(width: bg.size.width, height: spikeNode.size.height)
+      spikeNode.position = CGPoint(x: 0, y: self.frame.size.height / 6 )
+      spikeNode.physicsBody = SKPhysicsBody(texture: groundTexture, size: spikeNode.size)
+      spikeNode.physicsBody?.affectedByGravity = false
+      spikeNode.physicsBody?.dynamic = false
+      hotdog.physicsBody?.dynamic = true
+      bg.addChild(spikeNode)
+
       addChild(bg)
     }
     
-    var hotdogTexture = SKTexture(imageNamed: "hotdog")
+    let hotdogTexture = SKTexture(imageNamed: "hotdog")
     self.hotdog = SKSpriteNode(texture: hotdogTexture)
     self.hotdog.size = CGSizeMake(self.frame.size.width / 10, self.frame.size.height / 6)
     self.hotdog.zPosition = 100
-    self.hotdog.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
+    self.hotdog.position = CGPoint(x: self.frame.size.width / 2 - 100, y: self.frame.size.height / 2)
+    
+    self.hotdog.physicsBody = SKPhysicsBody(rectangleOfSize: self.hotdog.size)
+    
     self.addChild(self.hotdog)
     
+
     
   }
   
   override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+    
   }
   
+
   override func update(currentTime: CFTimeInterval) {
     /* Called before each frame is rendered */
     enumerateChildNodesWithName("background", usingBlock: { (node, stop) -> Void in
