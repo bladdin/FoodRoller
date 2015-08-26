ada1f7afb2fa770aa5b70a93ae3a55298b7f88ed
Mark Lin <m4rklin@comcast.net>
2015-08-26 11:05:23 -0700

    Add Help Text

--- Food Roller/Food Roller/HelpViewController.swift
+++ Food Roller/Food Roller/HelpViewController.swift
@@ -11,6 +11,13 @@ import UIKit
 class HelpViewController: UIViewController {
 
   @IBOutlet weak var howToPlayLabel: UILabel!
+  
+  @IBOutlet weak var howToPlayTextView: UITextView!
+  
+  override func viewWillAppear(animated: Bool) {
+    howToPlayTextView.text = "Last as long as you can without falling onto the cactuses.\n\nYou can jump from platforms or while in the air.  Jumping will cause you to drain your stamina, but landing on a platform will allow you to auto regenerate some stamina.\n\nTo jump, swipe anywhere on the screen in the opposite direction."
+  }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
       navigationController?.navigationBarHidden = false
