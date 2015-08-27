798932a819cd806fc19d95725895dd6d972c028f
Mark Lin <m4rklin@comcast.net>
2015-08-27 12:21:58 -0700

    here goes nothing.

--- Food Roller/Food Roller/GlobalVariables.swift
+++ Food Roller/Food Roller/GlobalVariables.swift
@@ -8,9 +8,20 @@
 
 import SpriteKit
 
-  var backgroundSpeed : CGFloat = 1
-  var spikeSpeed : CGFloat = 1
-  var gameOver = false
-  var musicVolume:Float = 0.5
-  var sfxVolume: Float = 0.5
+// hot dog textures
+
+var hotdogBun = SKTexture(imageNamed: "bun")
+var deadHotdogTexture = SKTexture(imageNamed: "deadHotDog")
+
+var backgroundSpeed : CGFloat = 1
+var spikeSpeed : CGFloat = 1
+var gameStop = false
+var gameStarted = false
+var bobWidth : Int!
+var lastBob :SKSpriteNode!
+var bobMinStart : Int!
+var bobMaxStart : Int!
+var moveAndRemove : SKAction!
+var musicVolume:Float = 0.5
+var sfxVolume: Float = 0.5
  
