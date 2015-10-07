782e9244f0963585c274215172a7250cdd576646
Mark Lin <m4rklin@comcast.net>
2015-10-07 14:56:03 -0700

    Fix Help and Fix Background Music

--- Food Roller/Food Roller/HelpViewController.swift
+++ Food Roller/Food Roller/HelpViewController.swift
@@ -15,7 +15,7 @@ class HelpViewController: UIViewController {
   @IBOutlet weak var howToPlayTextView: UITextView!
   
   override func viewWillAppear(animated: Bool) {
-    howToPlayTextView.text = "Last as long as you can without falling onto the cactuses, going too high, or falling too far behind.\n\nYou can jump from platforms or while in the air.  Jumping will cause you to drain your thumb stamina, but landing on a platform will allow you to rest your thumbs.\n\nTo jump, swipe anywhere on the screen in the opposite direction.\n\nGame created by Benjamin Laddin, Cathy Oun, Matthew Loh, Mark Lin.\nSpecial thanks to Brad Johnson and Nick McCardel of Code Fellows of Seattle\nCredit:\nArt by Shelly Oun and Ben Lin\nMusic by Benjamin TISSOT from Bensound.com\nSound effect Pain by thecheeseman from SoundBible.com\nSound effect Jump by snottyboy from SoundBible.com\nSound effect Squish Fart by timtube from SoundBible.com"
+    howToPlayTextView.text = "Last as long as you can without falling onto the cactuses, going too high, or falling too far behind.\n\nCollide with the pickles to score points.\n\nSwipe anywhere on the screen in the opposite direction to jump."
   }
   
     override func viewDidLoad() {
